378b12ff9805f3cc5e6317fc16b46b360d13b701
Aria Rana <aria.rana@ulead.ac.cr>
2020-10-02 20:28:23 -0600

    Initial commit

---
+++ src/VariableExamples.java
@@ -0,0 +1,10 @@
+public class VariableExamples {
+    public static void main(String[] args){
+
+        short shortNumber = 32767;
+        long longNumber = 2147483660L;
+        double doubleNonIntegerNumber = 2.5;
+
+    }
+
+}
